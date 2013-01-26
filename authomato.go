09aa78a62d950f6945c7ab06b0908fcf262a656f
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2013-01-25 20:46:56 -0800

    Command line parsing and logging

--- authomato.go
+++ authomato.go
@@ -7,19 +7,53 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
+var (
+	oauthConsumers OAuthConsumers
+)
+
 func main() {
-	startServer()
+	port := flag.Int("port", 8080, "specify port that the server will listen on")
+	flag.Parse()
+
+	// parse the names of configuration files if provided
+	var provFile, consFile string
+	if flag.NArg() > 0 {
+		if flag.NArg() > 1 {
+			consFile = flag.Arg(1)
+		} else {
+			consFile = "./oauth_consumers.json"
+		}
+		provFile = flag.Arg(0)
+	} else {
+		provFile = "./oauth_providers.json"
+	}
+
+	// load OAuth providers and consumers
+	if providers, err := loadOAuthProviders(provFile); err != nil {
+		log.Printf("Error while reading OAuth providers from %s: %+v", provFile, err)
+	} else if consumers, err := loadOAuthConsumers(consFile, providers); err != nil {
+		log.Printf("Error while reading OAuth consumers from %s: %+v", consFile, err)
+	} else {
+		oauthConsumers = consumers
+	}
+	log.Printf("Loaded %d OAuth consumer(s)", len(oauthConsumers))
+
+	startServer(*port)
 }
 
-// Starts the authomato server
-func startServer() {
+// Setup HTTP handlers and start the authomato server
+func startServer(port int) {
 	http.HandleFunc("/hello", helloHandler)
-	http.ListenAndServe(":8080", nil)
+
+	log.Printf("Listening on %d...", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
 
 // Handlers
@@ -46,9 +80,6 @@ type OAuthConsumer struct {
 
 type OAuthProviders map[string]OAuthProvider
 type OAuthConsumers map[string]OAuthConsumer
-
-var oauthProviders OAuthProviders
-var oauthConsumers OAuthConsumers
 
 func loadOAuthProviders(filename string) (OAuthProviders, error) {
 	b, err := ioutil.ReadFile(filename)
