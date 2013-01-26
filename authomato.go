6175c84a707c5a7e5b6d951e8ec0549a511b0705
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2013-01-25 22:32:36 -0800

    First step of OAuth flow, not tested

--- authomato.go
+++ authomato.go
@@ -11,16 +11,25 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
+
+	oauth "github.com/mrjones/oauth"
 )
 
 var (
+	serverProtocol string
+	serverDomain   string
+
 	oauthConsumers OAuthConsumers
+	oauthSessions  OAuthSessions = make(OAuthSessions)
 )
 
 func main() {
 	port := flag.Int("port", 8080, "specify port that the server will listen on")
+	domain := flag.String("domain", "127.0.0.1", "specify the server domain for callback URLs")
+	https := flag.Bool("https", false, "whether callback URLs should use HTTPS instead of HTTP")
 	flag.Parse()
 
 	// parse the names of configuration files if provided
@@ -46,12 +55,20 @@ func main() {
 	}
 	log.Printf("Loaded %d OAuth consumer(s)", len(oauthConsumers))
 
+	// remember server details and start it
+	if *https {
+		serverProtocol = "https"
+	} else {
+		serverProtocol = "http"
+	}
+	serverDomain = *domain
 	startServer(*port)
 }
 
 // Setup HTTP handlers and start the authomato server
 func startServer(port int) {
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/oauth/start", startOAuthFlow)
+	http.HandleFunc("/oauth/callback", callbackForOAuth)
 
 	log.Printf("Listening on port %d...", port)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
@@ -59,8 +76,50 @@ func startServer(port int) {
 
 // Handlers
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world!")
+func startOAuthFlow(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("app")
+	if len(name) == 0 {
+		http.Error(w, "'app' missing", http.StatusBadRequest)
+		return
+	}
+
+	consumer := oauthConsumers[name]
+	if consumer == nil {
+		http.Error(w, fmt.Sprint("invalid app '%s'", name), http.StatusNotFound)
+		return
+	}
+
+	// generate ID for this session
+	var sid string
+	for sid == "" || oauthSessions[sid] != nil {
+		sid = RandomString(24, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	}
+
+	// talk to the OAuth provider and obtain request token
+	c := oauth.NewConsumer(consumer.Key, consumer.Secret, oauth.ServiceProvider{
+		RequestTokenUrl:   consumer.Provider.RequestTokenUrl,
+		AuthorizeTokenUrl: consumer.Provider.AuthorizeUrl,
+		AccessTokenUrl:    consumer.Provider.AccessTokenUrl,
+	})
+	requestToken, url, err := c.GetRequestTokenAndUrl(
+		fmt.Sprint("%s://%s/oauth/callback?session=%s", serverProtocol, serverDomain, sid))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// save the session info and return ID + URL for the user
+	oauthSessions[sid] = &OAuthSession{
+		Id:           sid,
+		StartedAt:    time.Now(),
+		Consumer:     consumer,
+		RequestToken: requestToken,
+	}
+	fmt.Printf("%s %s", sid, url)
+}
+
+func callbackForOAuth(w http.ResponseWriter, r *http.Request) {
+	// ...
 }
 
 // Configuration data
@@ -83,12 +142,12 @@ type OAuthSession struct {
 	Id           string
 	StartedAt    time.Time
 	Consumer     *OAuthConsumer
-	RequestToken string
+	RequestToken *oauth.RequestToken
 }
 
-type OAuthProviders map[string]OAuthProvider // indexed by name
-type OAuthConsumers map[string]OAuthConsumer // indexed by name
-type OAuthSessions map[string]OAuthSession   // indexed by ID
+type OAuthProviders map[string]*OAuthProvider // indexed by name
+type OAuthConsumers map[string]*OAuthConsumer // indexed by name
+type OAuthSessions map[string]*OAuthSession   // indexed by ID
 
 func loadOAuthProviders(filename string) (OAuthProviders, error) {
 	b, err := ioutil.ReadFile(filename)
@@ -104,7 +163,7 @@ func loadOAuthProviders(filename string) (OAuthProviders, error) {
 
 	res := make(OAuthProviders)
 	for _, p := range providers {
-		res[p.Name] = p
+		res[p.Name] = &p
 	}
 	return res, nil
 }
@@ -123,7 +182,18 @@ func loadOAuthConsumers(filename string, oauthProviders OAuthProviders) (OAuthCo
 
 	res := make(OAuthConsumers)
 	for _, c := range consumers {
-		res[c.Name] = c
+		res[c.Name] = &c
 	}
 	return res, nil
+}
+
+// Utility functions
+
+func RandomString(length int, chars string) string {
+	res := ""
+	for i := 0; i < length; i++ {
+		k := rand.Intn(len(chars))
+		res += string(chars[k])
+	}
+	return res
 }
