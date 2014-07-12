7fafe2b64f5a3ca69132c5c29fb62feb7c54f151
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-07-11 23:54:46 -0700

    Bump version to 0.0.2

--- authomato.go
+++ authomato.go
@@ -25,7 +25,7 @@ import (
 	oauth "github.com/mrjones/oauth"
 )
 
-const version = "0.0.1"
+const version = "0.0.2"
 
 var (
 	port   = flag.Int("port", 8080, "specify port that the server will listen on")
